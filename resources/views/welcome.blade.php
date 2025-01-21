0b0f80bdcde26981869ee099e43ca512356f955a
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-21 12:37:13 +0100

    feat: arrow animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,14 @@
 @section('content')
 
 <div class="jumbotron">
-    <div class="container py-5">
-        <h1>Login to your company CRM</h1>
+    <div class="container py-5 d-flex flex-column justify-content-center align-items-center">
+        <h1 class="text-center">Welcome to the hub to manage all your costumers</h1>
+        <h2 class="text-center">Login to start</h2>
+        <div class="arrow">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 </div>
 @endsection
